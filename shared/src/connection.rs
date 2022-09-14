e8195564c3a261f5c5d1cba2ef6a29a7eefadb5a
0xb10c <b10c@b10c.me>
2022-09-14 12:02:39 +0200

    add: Network type with Display and From impls

--- shared/src/connection.rs
+++ shared/src/connection.rs
@@ -12,7 +12,7 @@ impl From<bcc_types::Connection> for Connection {
             peer_id: conn.id,
             addr: conn.addr(),
             conn_type: conn_type as i32,
-            network: conn.network,
+            network: Network::from(conn.network) as i32,
             net_group: conn.net_group,
         }
     }
@@ -25,6 +25,52 @@ impl fmt::Display for Connection {
             "Connection(id={}, addr={}, conn_type={}, network={}, net_group={})",
             self.peer_id, self.addr, self.conn_type, self.network, self.net_group
         )
+    }
+}
+
+// Must be the same as defined by Bitoin Core.
+// These are currently defined in https://github.com/bitcoin/bitcoin/blob/master/src/netaddress.h#L44
+
+// Bitcoin Core comment:
+// Addresses from these networks are not publicly routable on the global Internet.
+const BITCOINCORE_NET_UNROUTABLE: u32 = 0;
+const BITCOINCORE_NET_IPV4: u32 = 1;
+const BITCOINCORE_NET_IPV6: u32 = 2;
+const BITCOINCORE_NET_ONION: u32 = 3;
+const BITCOINCORE_NET_I2P: u32 = 4;
+const BITCOINCORE_NET_CJDNS: u32 = 5;
+// Bitcoin Core comment:
+// A set of addresses that represent the hash of a string or FQDN. We use
+// them in AddrMan to keep track of which DNS seeds were used.
+const BITCOINCORE_NET_INTERNAL: u32 = 6;
+
+impl From<u32> for Network {
+    fn from(network: u32) -> Self {
+        match network {
+            BITCOINCORE_NET_UNROUTABLE => Network::Unroutable,
+            BITCOINCORE_NET_IPV4 => Network::Ipv4,
+            BITCOINCORE_NET_IPV6 => Network::Ipv6,
+            BITCOINCORE_NET_ONION => Network::Onion,
+            BITCOINCORE_NET_I2P => Network::I2p,
+            BITCOINCORE_NET_CJDNS => Network::Cjdns,
+            BITCOINCORE_NET_INTERNAL => Network::Internal,
+            _ => Network::Unknown,
+        }
+    }
+}
+
+impl fmt::Display for Network {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Network::Unroutable => write!(f, "Unroutable"),
+            Network::Ipv4 => write!(f, "IPv4"),
+            Network::Ipv6 => write!(f, "IPv6"),
+            Network::Onion => write!(f, "Onion"),
+            Network::I2p => write!(f, "I2P"),
+            Network::Cjdns => write!(f, "CJDNS"),
+            Network::Internal => write!(f, "Internal"),
+            Network::Unknown => write!(f, "Unknown"),
+        }
     }
 }
 
