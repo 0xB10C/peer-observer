01a91b16b5ce4d411ed29817ee6f486e37d8f6f4
0xb10c <b10c@b10c.me>
2024-03-13 16:18:29 +0100

    add: multi-arch support - x86_64 and aarch64

--- extractor/build.rs
+++ extractor/build.rs
@@ -1,12 +1,18 @@
 use libbpf_cargo::SkeletonBuilder;
+use std::env;
+use std::path::Path;
 
 const SOURCE: &str = "./src/bpf/tracing.bpf.c";
 const DEST: &str = "./src/tracing.gen.rs";
 
 fn main() {
+    let arch = env::var("CARGO_CFG_TARGET_ARCH")
+        .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
+    let vmlinux_path = Path::new("./src/bpf/vmlinux").join(arch);
+    println!("using vmlinux path: {}", vmlinux_path.display());
     match SkeletonBuilder::new()
         .source(SOURCE)
-        .debug(true)
+        .clang_args(format!("-I {}", vmlinux_path.display()))
         .build_and_generate(DEST)
     {
         Ok(_) => (),
