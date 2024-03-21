0ffff626bc897534073bd24f6779aed02843ccf0
0xb10c <b10c@b10c.me>
2024-03-21 11:38:23 +0100

    add: hook up validation::block_connected tracepoint

---
+++ shared/src/validation.rs
@@ -0,0 +1,42 @@
+use crate::bitcoin::hashes::Hash;
+use crate::ctypes;
+use std::fmt;
+
+// structs are generated via the validation.proto file
+include!(concat!(env!("OUT_DIR"), "/validation.rs"));
+
+impl From<ctypes::ValidationBlockConnected> for BlockConnected {
+    fn from(connected: ctypes::ValidationBlockConnected) -> Self {
+        BlockConnected {
+            hash: connected.hash.to_vec(),
+            height: connected.height,
+            transactions: connected.transactions as i64,
+            inputs: connected.inputs,
+            sigops: connected.sigops as i64,
+            connection_time: connected.connection_time as i64,
+        }
+    }
+}
+
+impl fmt::Display for BlockConnected {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "BlockConnected(hash={}, height={}, transactions={}, inputs={}, sigops={}, time={}µs)",
+            bitcoin::BlockHash::from_slice(&self.hash).unwrap(),
+            self.height,
+            self.transactions,
+            self.inputs,
+            self.sigops,
+            self.connection_time,
+        )
+    }
+}
+
+impl fmt::Display for validation_event::Event {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            validation_event::Event::BlockConnected(connected) => write!(f, "{}", connected),
+        }
+    }
+}
