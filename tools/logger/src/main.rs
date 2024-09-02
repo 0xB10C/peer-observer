361701d4172af654f7d14adfa70155896a350e24
0xb10c <b10c@b10c.me>
2024-09-02 14:58:56 +0200

    refactor: logger -use log macros instead of println!

--- tools/logger/src/main.rs
+++ tools/logger/src/main.rs
@@ -7,7 +7,9 @@ use shared::clap;
 use shared::clap::Parser;
 use shared::event_msg;
 use shared::event_msg::event_msg::Event;
+use shared::log;
 use shared::prost::Message;
+use shared::simple_logger;
 
 /// Simple peer-observer tool that logs all received event messages
 #[derive(Parser, Debug)]
@@ -16,10 +18,17 @@ struct Args {
     // The extractor address the tool should connect to.
     #[arg(short, long, default_value = "tcp://127.0.0.1:8883")]
     address: String,
+    // The log level the tool should run on. Events are logged with
+    // the INFO log level. Valid log levels are "trace", "debug",
+    // "info", "warn", "error". See https://docs.rs/log/latest/log/enum.Level.html
+    #[arg(short, long, default_value_t = log::Level::Debug)]
+    log_level: log::Level,
 }
 
 fn main() {
     let args = Args::parse();
+
+    simple_logger::init_with_level(args.log_level).unwrap();
 
     let sub = Socket::new(Protocol::Sub0).unwrap();
     sub.dial(&args.address).unwrap();
@@ -34,7 +43,7 @@ fn main() {
         if let Some(event) = unwrapped {
             match event {
                 Event::Msg(msg) => {
-                    println! {
+                    log::info! {
                         "{} {} id={} (conn_type={:?}): {}",
                         if msg.meta.inbound { "<--"} else { "-->" },
                         if msg.meta.inbound { "from"} else { "to" },
@@ -44,22 +53,22 @@ fn main() {
                     };
                 }
                 Event::Conn(c) => {
-                    println! {
+                    log::info! {
                         "# CONN {}", c.event.unwrap()
                     };
                 }
                 Event::Addrman(a) => {
-                    println! {
+                    log::info! {
                         "@Addrman {}", a.event.unwrap()
                     };
                 }
                 Event::Mempool(m) => {
-                    println! {
+                    log::info! {
                         "$Mempool {}", m.event.unwrap()
                     };
                 }
                 Event::Validation(v) => {
-                    println! {
+                    log::info! {
                         "+Validation {}", v.event.unwrap()
                     };
                 }
