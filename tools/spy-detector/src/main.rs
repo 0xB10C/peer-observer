b86284168a63be24732567d7eb12f736326a6f6b
Yuvraj <yuvichh01@gmail.com>
2024-10-20 22:29:19 +0800

    small refractors

--- tools/spy-detector/src/main.rs
+++ tools/spy-detector/src/main.rs
@@ -16,7 +16,9 @@ use std::time::Duration;
 use std::time::Instant;
 
 const ADDRESS: &str = "tcp://127.0.0.1:8883";
-const STATS_INTERVAL: Duration = Duration::from_secs(120); // Duration(seconds) to print stats of peers
+const STATS_INTERVAL: Duration = Duration::from_secs(10); // Duration(seconds) to print stats of peers
+
+const LOG_TARGET: &str = "main";
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
@@ -28,9 +30,12 @@ struct Args {
 
 #[derive(Debug, Default)]
 struct PeerStats {
+    inv_tx_received: AtomicU32,
     inv_tx_sent: AtomicU32,                // TX(INV) sent by the peer
-    inv_wtx_received: AtomicU32,           // TX(INV) received by the peer
+    inv_wtx_sent: AtomicU32,               // WTX(INV) received by the peer
+    inv_wtx_received: AtomicU32,           // WTX(INV) received by the peer
     inv_witnesstx_received: AtomicU32,     // WitnessTX(INV) received by the peer
+    inv_witnesstx_sent: AtomicU32,         // WitnessTX(INV) sent by the peer
     getdata_witnesstx_sent: AtomicU32,     // WitnessTX(GETDATA) sent by the peer
     getdata_witnesstx_received: AtomicU32, // WitnessTX(GETDATA) received by the peer
     tx_sent: AtomicU32,                    // TX sent by the peer
@@ -39,7 +44,6 @@ struct PeerStats {
 }
 
 type PeerMap = Arc<DashMap<String, PeerStats>>;
-const LOG_TARGET: &str = "main";
 
 fn main() {
     let args = Args::parse();
@@ -49,6 +53,8 @@ fn main() {
     SimpleLogger::new()
         .init()
         .expect("Cannot setup Simple Logger.");
+
+    log::info!(target: LOG_TARGET, "Starting spy-detector...",);
 
     let sub = Socket::new(Protocol::Sub0).unwrap();
     sub.dial(ADDRESS).unwrap();
@@ -72,6 +78,8 @@ fn main() {
         }
     });
 
+    log::info!(target: LOG_TARGET, "Spy-detector started",);
+
     loop {
         let msg = sub.recv().unwrap();
         let message = event_msg::EventMsg::decode(msg.as_slice()).unwrap().event;
@@ -79,18 +87,28 @@ fn main() {
         if let Some(event) = message {
             match event {
                 Event::Msg(msg) => {
+                    let msg_type: u32;
+
+                    if msg.meta.inbound {
+                        msg_type = 0;
+                    } else {
+                        msg_type = 1;
+                    };
+
+                    let peer_id = msg.meta.peer_id;
+
                     if let Some(p2p_msg) = msg.msg {
                         match p2p_msg {
                             net_msg::message::Msg::Inv(_) => {
-                                process_inv_msg(&peer_map, &p2p_msg);
+                                process_inv_msg(&peer_map, &p2p_msg, msg_type, peer_id);
                             }
 
                             net_msg::message::Msg::Getdata(_) => {
-                                process_getdata_msg(&peer_map, &p2p_msg);
+                                process_getdata_msg(&peer_map, &p2p_msg, msg_type, peer_id);
                             }
 
                             net_msg::message::Msg::Tx(_) => {
-                                process_tx_msg(&peer_map, &p2p_msg);
+                                process_tx_msg(&peer_map, &p2p_msg, msg_type, peer_id);
                             }
                             _ => {}
                         }
@@ -108,30 +126,42 @@ fn main() {
     }
 }
 
-fn process_inv_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg) {
-    let peer_id = format!("{}", msg);
+fn process_inv_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg, msg_type: u32, peer_id: u64) {
     let stats = peer_map
-        .entry(peer_id.clone())
+        .entry(peer_id.to_string())
         .or_insert_with(PeerStats::default);
 
     if let net_msg::message::Msg::Inv(inv) = msg {
         for inv_item in &inv.items {
             match inv_item.inv_type() {
                 "Tx" => {
-                    println!("Tx recieved ");
-                    stats.inv_tx_sent.fetch_add(1, atomic::Ordering::Relaxed);
+                    if msg_type == 0 {
+                        stats
+                            .inv_tx_received
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    } else {
+                        stats.inv_tx_sent.fetch_add(1, atomic::Ordering::Relaxed);
+                    }
                 }
                 "WTx" => {
-                    println!("WTx recieved ");
-                    stats
-                        .inv_wtx_received
-                        .fetch_add(1, atomic::Ordering::Relaxed);
+                    if msg_type == 0 {
+                        stats
+                            .inv_wtx_received
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    } else {
+                        stats.inv_wtx_sent.fetch_add(1, atomic::Ordering::Relaxed);
+                    }
                 }
                 "WitnessTx" => {
-                    println!("WitnessTx recieved ");
-                    stats
-                        .inv_witnesstx_received
-                        .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                    if msg_type == 0 {
+                        stats
+                            .inv_witnesstx_received
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    } else {
+                        stats
+                            .inv_witnesstx_sent
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    }
                 }
                 _ => {} // Ignore other types
             }
@@ -139,38 +169,48 @@ fn process_inv_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg) {
     }
 }
 
-fn process_getdata_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg) {
-    let peer_id = format!("{}", msg);
+fn process_getdata_msg(
+    peer_map: &PeerMap,
+    msg: &net_msg::message::Msg,
+    msg_type: u32,
+    peer_id: u64,
+) {
     let stats = peer_map
-        .entry(peer_id.clone())
+        .entry(peer_id.to_string())
         .or_insert_with(PeerStats::default);
 
     if let net_msg::message::Msg::Inv(inv) = msg {
         for inv_item in &inv.items {
             match inv_item.inv_type() {
                 "WitnessTx" => {
-                    println!("WitnessTx recieved ");
-                    stats
-                        .getdata_witnesstx_received
-                        .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                    if msg_type == 0 {
+                        println!("WitnessTx recieved");
+                        stats
+                            .getdata_witnesstx_received
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    } else {
+                        println!("WitnessTx sent");
+                        stats
+                            .getdata_witnesstx_sent
+                            .fetch_add(1, atomic::Ordering::Relaxed);
+                    }
                 }
-
-                //todo: add functionality for witness_sent messages
                 _ => {} // Ignore other types
             }
         }
     }
 }
 
-fn process_tx_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg) {
-    let peer_id = format!("{}", msg);
+fn process_tx_msg(peer_map: &PeerMap, msg: &net_msg::message::Msg, msg_type: u32, peer_id: u64) {
     let stats = peer_map
-        .entry(peer_id.clone())
+        .entry(peer_id.to_string())
         .or_insert_with(PeerStats::default);
 
-    //todo: add functionality for tx_sent & tx_received messages
-
-    println!("{}", msg);
+    if msg_type == 0 {
+        stats.tx_received.fetch_add(1, atomic::Ordering::Relaxed);
+    } else {
+        stats.tx_sent.fetch_add(1, atomic::Ordering::Relaxed);
+    }
 }
 
 fn process_connection_event(peer_map: &PeerMap, event: &str) {
@@ -185,10 +225,11 @@ fn process_connection_event(peer_map: &PeerMap, event: &str) {
 
 fn print_peer_stats(peer_addr: &str, stats: &PeerStats) {
     println!(
-        "[{}] Peer ID {} stats:\n  INV TX sent: {:?}\n  INV WTX received: {:?}\n INV WitnessTX received: {:?}\n GetDATA WitnessTX received: {:?}\n  GETDATA WitnessTX received: {:?}\n  Tx sent: {:?}\n  Tx received: {:?}\n Last Activity: {:?}",
+        "[{}] Peer ID {} stats:\n  INV TX sent: {:?}\n  INV TX received: {:?}\n  INV WTX received: {:?}\n INV WitnessTX received: {:?}\n GetDATA WitnessTX received: {:?}\n  GETDATA WitnessTX received: {:?}\n  Tx sent: {:?}\n  Tx received: {:?}\n Last Activity: {:?}",
         chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
         peer_addr,
         stats.inv_tx_sent,
+        stats.inv_tx_received,
         stats.inv_wtx_received,
         stats.inv_witnesstx_received,
         stats.getdata_witnesstx_sent,
@@ -208,6 +249,8 @@ fn display_all_stats(peer_map: &PeerMap) {
     for item in map.iter() {
         let peer_id = item.key();
         let stats = item.value();
+
+        //println!("Key: {}\n", peer_id);
         print_peer_stats(&peer_id, &stats);
         println!("----------------------------------------");
     }
