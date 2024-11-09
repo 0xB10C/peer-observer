a9cd22459f49c047e21e9fed6ee6eff7c8eba09d
0xb10c <b10c@b10c.me>
2024-11-09 01:42:18 +0100

    replace: nng with nats subscribing in logger

--- tools/logger/src/main.rs
+++ tools/logger/src/main.rs
@@ -1,23 +1,20 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
-use shared::clap;
 use shared::clap::Parser;
 use shared::event_msg;
 use shared::event_msg::event_msg::Event;
 use shared::log;
-use shared::nng::options::protocol::pubsub::Subscribe;
-use shared::nng::options::Options;
-use shared::nng::{Protocol, Socket};
 use shared::prost::Message;
 use shared::simple_logger;
+use shared::{clap, nats};
 
 /// A peer-observer tool that logs all received event messages
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The extractor address the tool should connect to.
-    #[arg(short, long, default_value = "tcp://127.0.0.1:8883")]
-    address: String,
+    /// The NATS server address the tool should connect and subscribe to.
+    #[arg(short, long, default_value = "127.0.0.1:4222")]
+    nats_address: String,
     /// The log level the tool should run on. Events are logged with
     /// the INFO log level. Valid log levels are "trace", "debug",
     /// "info", "warn", "error". See https://docs.rs/log/latest/log/enum.Level.html
@@ -30,15 +27,13 @@ fn main() {
 
     simple_logger::init_with_level(args.log_level).unwrap();
 
-    let sub = Socket::new(Protocol::Sub0).unwrap();
-    sub.dial(&args.address).unwrap();
-
-    let all_topics = vec![];
-    sub.set_opt::<Subscribe>(all_topics).unwrap();
-
-    loop {
-        let msg = sub.recv().unwrap();
-        let unwrapped = event_msg::EventMsg::decode(msg.as_slice()).unwrap().event;
+    // TODO: handle unwraps
+    let nc = nats::connect(args.nats_address).unwrap();
+    let sub = nc.subscribe("*").unwrap();
+    for msg in sub.messages() {
+        let unwrapped = event_msg::EventMsg::decode(msg.data.as_slice())
+            .unwrap()
+            .event;
 
         if let Some(event) = unwrapped {
             match event {
