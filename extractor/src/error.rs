8cb542bd529903b5c82cf10a9d1eaf665bdf943e
0xb10c <b10c@b10c.me>
2024-11-09 01:32:52 +0100

    replace: nng with nats publishing in extractor

--- extractor/src/error.rs
+++ extractor/src/error.rs
@@ -8,7 +8,6 @@ use std::time::SystemTimeError;
 #[derive(Debug)]
 pub enum RuntimeError {
     LibBpf(libbpf_rs::Error),
-    Nng(shared::nng::Error),
     NoSuchBPFMap(String),
     NoSuchBPFProg(String),
     Io(IoError),
@@ -21,7 +20,6 @@ impl fmt::Display for RuntimeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             RuntimeError::LibBpf(e) => write!(f, "libbpf error: {}", e),
-            RuntimeError::Nng(e) => write!(f, "nanomsg error: {}", e),
             RuntimeError::Io(e) => write!(f, "IO error {}", e),
             RuntimeError::IntParse(e) => write!(f, "int parse error {}", e),
             RuntimeError::SystemTime(e) => write!(f, "system time error {}", e),
@@ -38,7 +36,6 @@ impl error::Error for RuntimeError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match *self {
             RuntimeError::LibBpf(ref e) => Some(e),
-            RuntimeError::Nng(ref e) => Some(e),
             RuntimeError::Io(ref e) => Some(e),
             RuntimeError::IntParse(ref e) => Some(e),
             RuntimeError::SystemTime(ref e) => Some(e),
@@ -52,12 +49,6 @@ impl error::Error for RuntimeError {
 impl From<libbpf_rs::Error> for RuntimeError {
     fn from(e: libbpf_rs::Error) -> Self {
         RuntimeError::LibBpf(e)
-    }
-}
-
-impl From<shared::nng::Error> for RuntimeError {
-    fn from(e: shared::nng::Error) -> Self {
-        RuntimeError::Nng(e)
     }
 }
 
---
+++ shared/src/nats_subjects.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+const NATS_SUBJECT_ADDRMAN: &str = "addrman";
+const NATS_SUBJECT_MEMPOOL: &str = "mempool";
+const NATS_SUBJECT_NETMSG: &str = "netmsg";
+const NATS_SUBJECT_NETCONN: &str = "netconn";
+const NATS_SUBJECT_VALIDATION: &str = "validation";
+
+pub enum Subject {
+    Addrman,
+    Mempool,
+    NetMsg,
+    NetConn,
+    Validation,
+}
+
+impl fmt::Display for Subject {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Subject::Addrman => write!(f, "{}", NATS_SUBJECT_ADDRMAN),
+            Subject::Mempool => write!(f, "{}", NATS_SUBJECT_MEMPOOL),
+            Subject::NetConn => write!(f, "{}", NATS_SUBJECT_NETCONN),
+            Subject::NetMsg => write!(f, "{}", NATS_SUBJECT_NETMSG),
+            Subject::Validation => write!(f, "{}", NATS_SUBJECT_VALIDATION),
+        }
+    }
+}
