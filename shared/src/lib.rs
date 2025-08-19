6da4546debf6a86fcfdd99311a959da09cb45a4f
0xb10c <b10c@b10c.me>
2025-08-19 16:41:38 +0200

    remove: dependency on nats create
    in favor of async_nats. Closes #192.

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -7,7 +7,6 @@ pub extern crate corepc_client;
 pub extern crate futures;
 pub extern crate lazy_static;
 pub extern crate log;
-pub extern crate nats;
 pub extern crate prometheus;
 pub extern crate prost;
 pub extern crate rand;
