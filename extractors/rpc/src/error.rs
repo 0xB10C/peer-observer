77fc8c580350adf96383f5c535133e53b78d6297
0xb10c <b10c@b10c.me>
2025-08-21 22:36:56 +0200

    add: properly return RuntimeErrors in rpc-extractor

--- extractors/rpc/src/error.rs
+++ extractors/rpc/src/error.rs
@@ -1,7 +1,10 @@
 use shared::async_nats;
+use shared::async_nats::ConnectErrorKind;
 use shared::corepc_client::client_sync::Error as RPCError;
+use shared::log::SetLoggerError;
 use std::error;
 use std::fmt;
+use std::io;
 use std::time::SystemTimeError;
 
 #[derive(Debug)]
@@ -46,5 +49,59 @@ impl From<SystemTimeError> for FetchOrPublishError {
 impl From<async_nats::error::Error<async_nats::client::PublishErrorKind>> for FetchOrPublishError {
     fn from(e: async_nats::error::Error<async_nats::client::PublishErrorKind>) -> Self {
         FetchOrPublishError::NatsPublish(e)
+    }
+}
+
+#[derive(Debug)]
+pub enum RuntimeError {
+    SetLogger(SetLoggerError),
+    Io(io::Error),
+    Corepc(shared::corepc_client::client_sync::Error),
+    NatsConnect(shared::async_nats::error::Error<ConnectErrorKind>),
+}
+
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RuntimeError::SetLogger(e) => write!(f, "set logger error {}", e),
+            RuntimeError::Io(e) => write!(f, "IO error {}", e),
+            RuntimeError::Corepc(e) => write!(f, "RPC client error {}", e),
+            RuntimeError::NatsConnect(e) => write!(f, "NATS connection error {}", e),
+        }
+    }
+}
+
+impl error::Error for RuntimeError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            RuntimeError::SetLogger(ref e) => Some(e),
+            RuntimeError::Io(ref e) => Some(e),
+            RuntimeError::Corepc(ref e) => Some(e),
+            RuntimeError::NatsConnect(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<SetLoggerError> for RuntimeError {
+    fn from(e: SetLoggerError) -> Self {
+        RuntimeError::SetLogger(e)
+    }
+}
+
+impl From<io::Error> for RuntimeError {
+    fn from(e: io::Error) -> Self {
+        RuntimeError::Io(e)
+    }
+}
+
+impl From<shared::corepc_client::client_sync::Error> for RuntimeError {
+    fn from(e: shared::corepc_client::client_sync::Error) -> Self {
+        RuntimeError::Corepc(e)
+    }
+}
+
+impl From<shared::async_nats::error::Error<ConnectErrorKind>> for RuntimeError {
+    fn from(e: shared::async_nats::error::Error<ConnectErrorKind>) -> Self {
+        RuntimeError::NatsConnect(e)
     }
 }
