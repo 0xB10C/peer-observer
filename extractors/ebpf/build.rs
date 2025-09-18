656a9b1ab7f4e492027c4a39b689c20a033a748f
0xb10c <b10c@b10c.me>
2025-09-18 12:56:25 +0200

    nix: use clang 15 for the ebpf build
    Required as on nixos-unstable:
    	error: clang_14 has been removed, as it is unmaintained and obsolete
    
    We can't simply use the clang_15 as we now need to use clang-unwrapped.
    
    Additionally, we now need kernel headers on NixOS.

--- extractors/ebpf/build.rs
+++ extractors/ebpf/build.rs
@@ -11,11 +11,34 @@ fn main() {
         .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
     let vmlinux_path = Path::new("./src/bpf/vmlinux").join(arch);
     println!("using vmlinux path: {}", vmlinux_path.display());
-    match SkeletonBuilder::new()
+
+    let mut binding = SkeletonBuilder::new();
+    let mut builder = binding
         .source(SOURCE)
-        .clang_args([OsStr::new("-I"), vmlinux_path.as_os_str()])
-        .build_and_generate(DEST)
-    {
+        .clang_args([OsStr::new("-I"), vmlinux_path.as_os_str()]);
+
+    // If KERNEL_HEADERS env var is set, add it as an include path
+    if let Ok(kernel_headers) = env::var("KERNEL_HEADERS") {
+        println!(
+            "using kernel headers path from KERNEL_HEADERS: {}",
+            kernel_headers
+        );
+        builder = builder.clang_args([
+            OsStr::new("-I"),
+            vmlinux_path.as_os_str(),
+            // don't include any standard includes; this helps in the Ubuntu CI
+            // when building the package with Nix
+            OsStr::new("-nostdinc"),
+            OsStr::new("-v"),
+            OsStr::new("-I"),
+            OsStr::new(&kernel_headers),
+        ]);
+    } else {
+        println!("KERNEL_HEADERS env var not set; skipping kernel headers include");
+        builder = builder.clang_args([OsStr::new("-I"), vmlinux_path.as_os_str()]);
+    }
+
+    match builder.build_and_generate(DEST) {
         Ok(_) => (),
         Err(e) => {
             eprintln!("Failed to build BPF skeleton:");
