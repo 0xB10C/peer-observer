ca4f0167185f3f45e2be9091d2e69c1d9ecbd19e
Maycon Fabio <m.vinnicyus@gmail.com>
2025-09-23 00:19:59 -0300

    feat: Add log-extractor package and implement log reading from pipe

--- extractors/log/src/lib.rs
+++ extractors/log/src/lib.rs
@@ -1,18 +1,13 @@
-use shared::clap::{ArgGroup, Parser};
-use shared::corepc_client::client_sync::Auth;
-use shared::corepc_client::client_sync::v29::Client;
-use shared::log;
-use shared::nats_subjects::Subject;
-use shared::prost::Message;
-use shared::protobuf::event_msg::EventMsg;
-use shared::protobuf::event_msg::event_msg::Event;
+use shared::clap::{Parser};
+use shared::{log};
 use shared::tokio::sync::watch;
-use shared::tokio::time::{self, Duration};
-use shared::{async_nats, clap};
+use shared::{clap};
+use shared::tokio::fs::File;
+use shared::tokio::io::{AsyncBufReadExt, BufReader};
 
 mod error;
 
-use error::{FetchOrPublishError, RuntimeError};
+use error::{RuntimeError};
 
 /// TODO: Update
 /// The peer-observer log-extractor periodically queries data from the
@@ -44,5 +39,45 @@ impl Args {
 }
 
 pub async fn run(args: Args, mut shutdown_rx: watch::Receiver<bool>) -> Result<(), RuntimeError> {
+    // log::debug!("Connecting to NATS server at {}..", args.nats_address);
+    // let nats_client = async_nats::connect(&args.nats_address).await?;
+    // log::info!("Connected to NATS server at {}", &args.nats_address);
+
+    log::info!("Opening bitcoind log pipe...");
+    let file = File::open("bitcoind_pipe").await?;
+    log::info!("Opened bitcoind log pipe");
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
+    loop {
+        shared::tokio::select! {
+            line = lines.next_line() => {
+                match line {
+                    Ok(Some(line)) => println!("{}", line),
+                    Ok(None) => {
+                        log::info!("End of log pipe reached.");
+                        break;
+                    }
+                    Err(e) => return Err(e.into()),
+                }
+            },
+            res = shutdown_rx.changed() => {
+                match res {
+                    Ok(_) => {
+                        if *shutdown_rx.borrow() {
+                            log::info!("log-extractor received shutdown signal.");
+                            break;
+                        }
+                    }
+                    Err(_) => {
+                        // all senders dropped -> treat as shutdown
+                        log::warn!("The shutdown notification sender was dropped. Shutting down.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     Ok(())
 }
