a21ee0c320f90d60522c092d0eb658c47906d786
0xb10c <b10c@b10c.me>
2025-10-28 15:40:15 +0100

    add: category and timestamp to logger log messages

--- shared/src/protobuf/log_extractor.rs
+++ shared/src/protobuf/log_extractor.rs
@@ -1,7 +1,5 @@
 use std::fmt;
 
-use crate::protobuf::log_extractor;
-
 // structs are generated via the log_extractor.proto file
 include!(concat!(env!("OUT_DIR"), "/log_extractor.rs"));
 
@@ -21,11 +19,11 @@ impl fmt::Display for BlockConnectedLog {
     }
 }
 
-impl fmt::Display for log_extractor::log_event::Event {
+impl fmt::Display for log_event::Event {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            log_extractor::log_event::Event::UnknownLogMessage(message) => write!(f, "{}", message),
-            log_extractor::log_event::Event::BlockConnectedLog(block) => write!(f, "{}", block),
+            log_event::Event::UnknownLogMessage(message) => write!(f, "{}", message),
+            log_event::Event::BlockConnectedLog(block) => write!(f, "{}", block),
         }
     }
 }
